486c1fe1687b894754c244f7ff26b0963b44eaf3
fangjianwei <fangjianwei2018@gmail.com>
2021-07-02 14:28:32 +0800

    Added signal.

--- main.go
+++ main.go
@@ -7,38 +7,38 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"syscall"
 )
 
 func main() {
 
 	flag.Parse()
 
-	r,err := runner.NewRunner(flag.Args())
-	if err != nil{
+	r, err := runner.NewRunner(flag.Args())
+	if err != nil {
 		log.Fatal(err)
 	}
 	defer r.Cleanup()
 
 	w := watcher.NewWatcher(r)
 
-
 	go func() {
 		err := r.Exec()
-		if err != nil{
+		if err != nil {
 			log.Fatal(err)
 		}
 	}()
 
 	go func() {
 		err := w.Start()
-		if err != nil{
+		if err != nil {
 			log.Fatal(err)
 		}
 	}()
 
 	sigChan := make(chan os.Signal, 1)
 
-	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM, syscall.SIGHUP)
 
 	<-sigChan
 }
